4e539b111791f7a14500afd5638a3998765de4f2
Abdellatif Bouyatas <abouyata@c3r5p9.1337.ma>
2023-12-08 11:36:42 +0100

    done

--- main.c
+++ main.c
@@ -1,13 +1,12 @@
 #include "ft_printf.h"
 #include <stdio.h>
-#include <stdlib.h>
 int	main()
 {
 	int f = 59;
 	void *p;
-	int d = ft_printf("%d %s %i %u %p %x %%%%%%%%%%%%%%%%%d/#",f, NULL,142,15,&p,145236,atoi("654"));
+	int d = ft_printf("%d %s %i %u %p %x %",f, NULL,142,15,&p,145236);
 	ft_printf("-->%d\n",d);
-	int d1 = printf("%d %s %i %u %p %x %%%%%%%%%%%%%%%%%d/#",f, NULL,142,15,&p,145236,atoi("654"));
+	int d1 = printf("%d %s %i %u %p %x %",f, NULL,142,15,&p,145236);
 	printf("-->%d\n",d1);
 	return 0;
 }
